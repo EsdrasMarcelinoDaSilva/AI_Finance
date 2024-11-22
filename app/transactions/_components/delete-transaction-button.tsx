b676c223d92abb8a30852ddf9905b2d246f29023
EsdrasMarcelinoDaSilva <94310938+EsdrasMarcelinoDaSilva@users.noreply.github.com>
2024-11-22 00:41:04 -0300

    feat: ensure transaction can be deleted

---
+++ app/transactions/_components/delete-transaction-button.tsx
@@ -0,0 +1,60 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/app/_components/ui/alert-dialog";
+import { Button } from "@/app/_components/ui/button";
+import { AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
+import { TrashIcon } from "lucide-react";
+import { deleteTransaction } from "../_actions/delete-transaction";
+import { toast } from "sonner";
+
+interface DeleteTransactionButtonProps {
+  transactionId: string;
+}
+
+const DeleteTransactionButton = ({
+  transactionId,
+}: DeleteTransactionButtonProps) => {
+  const handleDeleteClick = async () => {
+    try {
+      await deleteTransaction({ transactionId });
+      toast.success("Transação deletada com sucesso");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao deletar transação");
+    }
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <TrashIcon />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Voçê Deseja realmente deletar essa transação?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Essa ação nao pode ser desfeita.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDeleteClick}>
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteTransactionButton;

6131a23e318a7b97764fe38b556698e715781e54
EsdrasMarcelinoDaSilva <94310938+EsdrasMarcelinoDaSilva@users.noreply.github.com>
2025-02-05 12:19:24 -0300

    feat: update all

---
+++ prisma/migrations/20250205151816_update_db_neon/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Transaction` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Transaction";
+
+-- CreateTable
+CREATE TABLE "Trasaction" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "category" "TransactionCategory" NOT NULL,
+    "paymentMethod" "transactionPaymentMethod" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Trasaction_pkey" PRIMARY KEY ("id")
+);
